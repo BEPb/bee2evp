7ad047ee71011dda2af6128ded51bcc75f5df048
BEPb <andrej.marinchenko@gmail.com>
2023-09-03 10:13:21 +0300

    upd add bee2_test.py

--- test/draft.py
+++ test/draft.py
@@ -1,102 +1,56 @@
+import socket
+import ssl
 from openssl import openssl
-import subprocess
-from belt import *
+import os
 from bign import *
-from settings import *
-import sys, os, shutil
-import signal
 import tempfile
-import re
-import threading
-import time
-from os.path import expanduser
-home = expanduser("~")
 
-import traceback
-from openssl import openssl
-from settings import hex_encoder, b64_encoder, hex_decoder, b64_decoder
-
-def beltBlockEncr(block, key):
-	assert len(block) * 8 == 128
-	print('block - ', block)
-	print('key - ', key)
-
-	plain = b64_encoder(block)[0].decode()
-	key = hex_encoder(key)[0].decode()
-	key_bitlen = len(key) * 4
-	print('plain - ', plain)
-	print('hex_key - ', key)
-	print('key_bitlen', key_bitlen)
-
-	prefix = 'echo ' + plain[:-1] + ' | python -m base64 -d |'
-	cmd = 'enc -e -engine bee2evp -belt-ecb{} -nosalt -nopad -e -K {}'.format(key_bitlen, key)
-	print('prefix - ', prefix)
-	print('cmd - ', cmd)
-	retcode, block, er__ = openssl(cmd, prefix)
-	print('retcode - ', retcode)
-	print('block - ', block)
-	print('er__ - ', er__)
-	return block
-
-fail = False
-def test_result(test_name, retcode):
-	global fail
-	if(retcode == 1):
-		sys.stdout.write(test_name + ': ')
-		print_colored('success', bcolors.OKGREEN)
-	else:
-		sys.stdout.write(test_name + ': ')
-		print_colored('fail', bcolors.FAIL)
-		fail = True
-
-def test_version():
-	retcode, out, __ = openssl('version')
-	test_result('version', retcode)
-	print(out.decode())
-
-def test_engine():
-	retcode, out, er__ = openssl('engine -c -t bee2evp')
-	test_result('engine', retcode)
-	print(out.decode())
-
-def test_belt():
-	#Block (|X| = 128)
-	#A.1 Encrypt
-	print('data - b194bac80a08f53b366d008e584a5de4')
-	block = hex_decoder('b194bac80a08f53b366d008e584a5de4')[0]
-	print('hexblock - ', hex_decoder('b194bac80a08f53b366d008e584a5de4'))
-	print('hexblock[0] - ', block)
-	key = hex_decoder('e9dee72c8f0c0fa62ddb49f46f739647'
-					  '06075316ed247a3739cba38303a98bf6')[0]
-	print('hexkey - ', hex_decoder('e9dee72c8f0c0fa62ddb49f46f73964706075316ed247a3739cba38303a98bf6'))
-	print('hexkey[0] - ', key)
-	print(len('b194bac80a08f53b366d008e584a5de4'))
-	print(len('e9dee72c8f0c0fa62ddb49f46f73964706075316ed247a3739cba38303a98bf6'))
-	print('bytes_hex_block - ', bytes(block))
-	print('bytes_hex_key - ', bytes(key))
-	block = beltBlockEncr(bytes(block), bytes(key))
-	print('encr_block - ', block)
-	res = hex_encoder(block)[0].decode() == '69cca1c93557c9e3d66bc3e0fa88fa6e'
-	print('hex_encoder - ', hex_encoder(block)[0].decode())
-	print('res - ', res)
-	test_result('Block Encrypt', res)
-
-	#A.4 Decrypt
-	block = hex_decoder('e12bdc1ae28257ec703fccf095ee8df1')[0]
-	key = hex_decoder('92bd9b1ce5d141015445fbc95e4d0ef2'
-					  '682080aa227d642f2687f93490405511')[0]
-	block = beltBlockDecr(bytes(block), bytes(key))
-	res = hex_encoder(block)[0].decode() == '0dc5300600cab840b38448e5e993f421'
-	test_result('Block Decrypt', res)
+def btls_gen_privkey(privfile, curve):
+	cmd = 'genpkey -engine bee2evp -algorithm bign -pkeyopt params:{} -out {}'.format(curve, privfile)
+	print(cmd)
+	retcode, block, er__ = openssl(cmd)
+	print(retcode)
+	print(block)
+	print(er__)
 
 
+def btls_issue_cert(privfile, certfile):
+	cmd = ('req -x509 -subj "/CN=www.example.org/O=BCrypto/C=BY/ST=MINSK" -new -key {} -nodes -out {}'.format(privfile, certfile))
+	print(cmd)
+	retcode, block, er__ = openssl(cmd)
+	print(retcode)
+	print(block)
+	print(er__)
+
+def main():
+	tmpdirname = tempfile.mkdtemp()
+	server_log_file = os.path.join(tmpdirname, 's_log.txt')
+	curve = 'bign-curve256v1'
+	priv = os.path.join(tmpdirname, '{}.key'.format(curve))
+	btls_gen_privkey(priv, curve)
+
+	cert = os.path.join(tmpdirname, 'cert.pem')
+	btls_issue_cert(priv, cert)
+	context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+	context.load_cert_chain(certfile=cert, keyfile=priv)
+
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.bind(('172.18.0.3', 2023))
+		s.listen()
+		print("Start")
+
+
+		with context.wrap_socket(s, server_side=True) as wrapped_socket:
+			conn, addr = wrapped_socket.accept()
+			print('Connected by', addr)
+
+			with open('file.txt', 'rb') as f:
+				data = f.read()
+				conn.sendall(data)
+
+	finally:
+		s.close()
 
 if __name__ == '__main__':
-	test_version()
-	test_engine()
-	test_belt()
-	# test_bign()
-	# test_belt_kwp_dwp()
-	# test_btls()
-	if (fail == True):
-		sys.exit(1)
+	main()
--- test/server.py
+++ test/server.py
@@ -1,3 +1,5 @@
+import socket
+import ssl
 from openssl import openssl
 import os
 from bign import *
@@ -22,30 +24,47 @@ def btls_issue_cert(privfile, certfile):
 
 def btls_server_cert(tmpdirname, server_log_file, curve, psk=False):
 
-    priv = os.path.join(tmpdirname, '{}.key'.format(curve))
-    btls_gen_privkey(priv, curve)
+	priv = os.path.join(tmpdirname, '{}.key'.format(curve))
+	btls_gen_privkey(priv, curve)
 
-    cert = os.path.join(tmpdirname, 'cert.pem')
-    btls_issue_cert(priv, cert)
+	cert = os.path.join(tmpdirname, 'cert.pem')
+	btls_issue_cert(priv, cert)
 
-    if psk:
-        cmd = 's_server -key {} -cert {} -tls1_2 -psk 123456 -psk_hint 123'.format(priv, cert)
-    else:
-        cmd = 's_server -key {} -cert {} -tls1_2 -accept 2034'.format(priv, cert)
+	if psk:
+		cmd = 's_server -key {} -cert {} -tls1_2 -psk 123456 -psk_hint 123'.format(priv, cert)
+	else:
+		cmd = 's_server -key {} -cert {} -tls1_2 -accept 2034'.format(priv, cert)
 
 	global server_cert
 	server_cert = openssl(cmd, type_=1)
 	print('certificate', server_cert)
-    print('Server start')
+	print('Server start')
 
 def main():
-    tmpdirname = tempfile.mkdtemp()
-    server_log_file = os.path.join(tmpdirname, 's_log.txt')
-    curve = 'bign-curve256v1'
+	tmpdirname = tempfile.mkdtemp()
+	server_log_file = os.path.join(tmpdirname, 's_log.txt')
+	curve = 'bign-curve256v1'
+	# btls_server_cert(tmpdirname, server_log_file, curve)
+    # print('Server runed!')
+	priv = os.path.join(tmpdirname, '{}.key'.format(curve))
+	btls_gen_privkey(priv, curve)
 
-    btls_server_cert(tmpdirname, server_log_file, curve)
+	cert = os.path.join(tmpdirname, 'cert.pem')
+	btls_issue_cert(priv, cert)
+	context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+	context.load_cert_chain(certfile=cert, keyfile=priv)
 
-    print('Server runed!')
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.bind(('172.18.0.3', 2023))
+		s.listen()
+
+		with context.wrap_socket(s, server_side=True) as wrapped_socket:
+			conn, addr = wrapped_socket.accept()
+			print('Connected by', addr)
+
+			with open('file.txt', 'rb') as f:
+				data = f.read()
+				conn.sendall(data)
 
 if __name__ == '__main__':
-    main()
+	main()
